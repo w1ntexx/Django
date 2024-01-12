a77e4bf7c35c46e03c632b7ce112c31bda2ce176
w1ntexx <h1kky8989@gmail.com>
2024-01-12 14:00:52 +0700

    add model

--- sitetimix/cats/models.py
+++ sitetimix/cats/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+# https://docs.djangoproject.com/en/4.2/ref/models/fields/
 
-# Create your models here.
+class Cat(models.Model):
+    title = models.CharField(max_length=255)  # VARCHAR(255)
+    content = models.TextField(blank=True)  # TEXT, поле может быть пустым
+    time_create = models.DateTimeField(auto_now_add=True)  # НОВАЯ УНИКАЛЬНАЯ запись будет добавлятся автоматически
+    time_update = models.DateTimeField(auto_now=True)  # Меняет ТЕКУЩУЮ запись
+    is_published = models.BooleanField(default=True)  # is_published = True по дефолту
--- sitetimix/sitetimix/settings.py
+++ sitetimix/sitetimix/settings.py
@@ -73,9 +73,9 @@ WSGI_APPLICATION = "sitetimix.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+DATABASES = { 
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
+        "ENGINE": "django.db.backends.sqlite3", # по умолчанию sqlite3 
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
