b4bace347554f6229feab6fdd8f035a2253add71
w1ntexx <h1kky8989@gmail.com>
2024-01-12 15:48:54 +0700

    add ipython, django-extensions

---
+++ sitetimix/cats/migrations/__init__.py
@@ -0,0 +1,4 @@
+# python manage.py makemigrations  -> СОЗДАНИЕ файлов миграций
+# python manage.py sqlmigrate {name_app} {NUMBER}
+# python manage.py migrate -> ПРИМЕНЕНИЕ миграция
+# cats_cat = cats -> назваие приложения, cat -> model
