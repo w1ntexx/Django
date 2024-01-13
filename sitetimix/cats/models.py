e7b75f39919c1e6d0eaf317bc04fcad6cf65b1d5
w1ntexx <h1kky8989@gmail.com>
2024-01-13 17:40:02 +0700

    class Meta, +db.objects

--- sitetimix/cats/models.py
+++ sitetimix/cats/models.py
@@ -2,11 +2,24 @@ from django.db import models
 
 # https://docs.djangoproject.com/en/4.2/ref/models/fields/
 
+
 class Cat(models.Model):
-    # Последовательность полей по умолчанию будет последовательной 
-    
-    title = models.CharField(max_length=255)  # однострочный 
+    # Последовательность полей по умолчанию будет последовательной
+
+    title = models.CharField(max_length=255)  # однострочный
     content = models.TextField(blank=True)  # многострочный TEXT, поле может быть пустым
     time_create = models.DateTimeField(auto_now_add=True)  # НОВАЯ УНИКАЛЬНАЯ запись будет добавлятся автоматически
     time_update = models.DateTimeField(auto_now=True)  # Меняет ТЕКУЩУЮ запись
     is_published = models.BooleanField(default=True)  # is_published = True по дефолту
+
+    def __str__(self) -> str:
+        return self.title
+
+    # https://docs.djangoproject.com/en/4.2/ref/models/options/
+    class Meta:
+        ordering = ["-time_create"]
+        indexes = [
+            models.Index(fields=["-time_create"]),  # Ускоряет поиск по полю, но увеличивает объем данных
+        ]
+        
+        
